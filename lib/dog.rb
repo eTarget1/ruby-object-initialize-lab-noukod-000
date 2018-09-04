7888f4c1bd31520aac21878c84f09b393ca844fd
clifford <clifford@noukod.com>
2018-09-04 19:17:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,6 +3,26 @@ class Dog
     @name = name
 
   end
-
+  def breed
+    @breed
+  end
+end
 
 end
+
+
+
+class Dog
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
+ 
+snoopy = Dog.new #=> #<Dog:0x007f970a2edfd0>
+snoopy.breed #=> nil
+snoopy.breed = "Beagle"
+snoopy.breed
