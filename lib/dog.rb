f472fadb13afb9075d87cecdceb0dd06baf46185
clifford <clifford@noukod.com>
2018-09-04 19:24:39 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,10 @@ class Dog
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  
+  end
+  def name=(name, breed = "Mutt")
+    @Name = name
+    @breed = breed
   end
 end
 end
